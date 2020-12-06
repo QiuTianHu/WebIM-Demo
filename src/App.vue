b28aba9a151516a74493a0680a4d7c1e3b1e715f
fish-tian <tshl359250296@qq.com>
2020-12-06 16:12:13 +0800

    使用UI库vuetify重写界面

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,102 @@
 <template>
-  <div id="app">
-    <LoginBox v-if="LoginBoxSeen" />
-    <FriendsBox v-bind:friends="friends" />
-    <ChatBox />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-btn text> 联系人 </v-btn>
+      <v-btn text> 会话 </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-avatar size="36">
+        <img src="@/assets/avatar1.jpeg" alt="avatar1" />
+      </v-avatar>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        登录
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+            <!-- v-card 里是联系人卡片 -->
+            <v-card>
+              <v-list subheader>
+                <v-subheader>联系人</v-subheader>
+                <!-- <v-list-item> 里是单个联系人 -->
+                <v-list-item>
+                  <v-list-item-avatar size="36px">
+                    <img src="@/assets/avatar1.jpeg" alt="avatar1" />
+                  </v-list-item-avatar>
+                  <v-list-item-content>尼古拉斯·磊</v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-avatar size="36px">
+                    <img src="@/assets/avatar2.jpeg" alt="avatar1" />
+                  </v-list-item-avatar>
+                  <v-list-item-content>莱昂纳多·虎</v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-avatar size="36px">
+                    <img src="@/assets/avatar3.jpeg" alt="avatar1" />
+                  </v-list-item-avatar>
+                  <v-list-item-content>克里斯蒂安·恒</v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+          <v-spacer></v-spacer>
+
+          <!-- v-card 里是对话框卡片 -->
+          <v-col>
+            <v-card>
+              <v-list subheader>
+                <v-subheader>尼古拉斯·磊</v-subheader>
+                <!-- <v-list-item> 里是单个信息 -->
+                <v-list-item>
+                  <v-list-item-avatar size="36px">
+                    <img src="@/assets/avatar1.jpeg" alt="avatar1" />
+                  </v-list-item-avatar>
+                  <v-list-item-content>你好</v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-avatar size="36px">
+                    <img src="@/assets/avatar1.jpeg" alt="avatar1" />
+                  </v-list-item-avatar>
+                  <v-list-item-content>在么</v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-content class="text-right align-self-start">
+                    <v-list-item-title> 你是谁？ </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-avatar size="36px">
+                    <img src="@/assets/avatar1.jpeg" alt="avatar1" />
+                  </v-list-item-avatar>
+                  <v-list-item-content>我是尼古拉斯·磊</v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import LoginBox from "./components/LoginBox.vue";
-import FriendsBox from "./components/FriendsBox.vue";
-import ChatBox from "./components/ChatBox.vue";
-
 export default {
   name: "App",
-  components: {
-    LoginBox,
-    FriendsBox,
-    ChatBox,
-  },
-  method: {
 
-  },
-  data: function () {
-    return {
-      LoginBoxSeen: true,
-    };
-  },
+  components: {},
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  color: #2c3e50;
-}
-</style>
